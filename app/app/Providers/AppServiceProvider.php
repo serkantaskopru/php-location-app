ad3aaaba3eaa252373fae4c1a68769bded750771
Serkan Taskopru <serkan.taskopru13@gmail.com>
2024-12-14 13:26:33 +0300

    fix: Schema string length fix for migration

--- app/app/Providers/AppServiceProvider.php
+++ app/app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Interfaces\LocationInterface;
 use App\Repositories\LocationRepository;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,6 +22,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Schema::defaultStringLength(191);
     }
 }
